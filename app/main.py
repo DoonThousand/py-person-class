e49b3133db2fc181f3330153d02cc9aa5eae39a7
Admin <valerasarkisan285@gmail.com>
2025-11-04 20:49:54 +0200

    py-person-class

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
 class Person:
-
     people = {}
 
     def __init__(self, name: str, age: int) -> None:
@@ -10,15 +9,17 @@ class Person:
 
 def create_person_list(people: list) -> list:
     Person.people.clear()
-    for i in people:
-        name = i["name"]
-        age = i["age"]
-        Person(name, age)
 
-    for i in people:
-        family = Person.people[i["name"]]
-        if i.get("wife"):
-            family.wife = Person.people[i["wife"]]
-        if i.get("husband"):
-            family.husband = Person.people[i["husband"]]
+    _ = [
+        Person(person_data["name"], person_data["age"])
+        for person_data in people
+    ]
+
+    for person_data in people:
+        family = Person.people[person_data["name"]]
+        if person_data.get("wife"):
+            family.wife = Person.people[person_data["wife"]]
+        if person_data.get("husband"):
+            family.husband = Person.people[person_data["husband"]]
+
     return list(Person.people.values())
